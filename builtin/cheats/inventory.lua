ad871cacafdf3485a73d88384fdcf449cefe6481
ProunceDev <flamingrccars@gmail.com>
2025-02-13 20:56:27 -0600

    Make Scaffold and Auto Refill work MUCH better

--- builtin/cheats/inventory.lua
+++ builtin/cheats/inventory.lua
@@ -9,26 +9,27 @@ strip_craft_act:craft("current_player")
 local strip_move_back_act = InventoryAction("move")
 strip_move_back_act:from("current_player", "craftresult", 1)
 
-minetest.register_globalstep(function(dtime)
-	local player = minetest.localplayer
+core.register_globalstep(function(dtime)
+	local player = core.localplayer
 	if not player then return end
 	local item = player:get_wielded_item()
-	local itemdef = minetest.get_item_def(item:get_name())
+	local itemdef = core.get_item_def(item:get_name())
 	local wieldindex = player:get_wield_index()
 	-- AutoRefill
-	if minetest.settings:get_bool("autorefill") and itemdef then
+	if (core.settings:get_bool("autorefill") or core.settings:get_bool("scaffold") or core.settings:get_bool("scaffold_plus")) and itemdef then
 		local space = item:get_free_space()
-		local i = minetest.find_item(item:get_name(), wieldindex + 1)
+		local i = core.find_additional_items(item:get_name(), wieldindex+1)
+		local total_items = core.get_total_items(item:get_name())
 		if i and space > 0 then
 			local move_act = InventoryAction("move")
-			move_act:to("current_player", "main", wieldindex)
+			move_act:to("current_player", "main", wieldindex + 1)
 			move_act:from("current_player", "main", i)
 			move_act:set_count(space)
 			move_act:apply()
 		end
 	end
 	-- AutoPlanks (Strip in DF)
-	if minetest.settings:get_bool("autoplanks") then
+	if core.settings:get_bool("autoplanks") then
 		if itemdef and itemdef.groups.tree and player:get_control().place then
 			strip_move_act:from("current_player", "main", wieldindex)
 			strip_move_back_act:to("current_player", "main", wieldindex)
@@ -38,9 +39,9 @@ minetest.register_globalstep(function(dtime)
 		end
 	end
 	-- AutoEject
-	if minetest.settings:get_bool("autoeject") then
-		local list = (minetest.settings:get("eject_items") or ""):split(",")
-		local inventory = minetest.get_inventory("current_player")
+	if core.settings:get_bool("autoeject") then
+		local list = (core.settings:get("eject_items") or ""):split(",")
+		local inventory = core.get_inventory("current_player")
 		for index, stack in pairs(inventory.main) do
 			if table.indexof(list, stack:get_name()) ~= -1 then
 				drop_action:from("current_player", "main", index)
@@ -50,7 +51,7 @@ minetest.register_globalstep(function(dtime)
 	end
 end)
 
-minetest.register_list_command("eject", "Configure AutoEject", "eject_items")
+core.register_list_command("eject", "Configure AutoEject", "eject_items")
 
 -- Enderchest
 
@@ -65,10 +66,10 @@ function get_itemslot_bg(x, y, w, h)
 end
 
 local enderchest_formspec = "size[9,8.75]"..
-	"label[0,0;"..minetest.formspec_escape(minetest.colorize("#313131", "Ender Chest")).."]"..
+	"label[0,0;"..core.formspec_escape(core.colorize("#313131", "Ender Chest")).."]"..
 	"list[current_player;enderchest;0,0.5;9,3;]"..
 	get_itemslot_bg(0,0.5,9,3)..
-	"label[0,4.0;"..minetest.formspec_escape(minetest.colorize("#313131", "Inventory")).."]"..
+	"label[0,4.0;"..core.formspec_escape(core.colorize("#313131", "Inventory")).."]"..
 	"list[current_player;main;0,4.5;9,3;9]"..
 	get_itemslot_bg(0,4.5,9,3)..
 	"list[current_player;main;0,7.74;9,1;]"..
@@ -76,17 +77,17 @@ local enderchest_formspec = "size[9,8.75]"..
 	"listring[current_player;enderchest]"..
 	"listring[current_player;main]"
 
-function minetest.open_enderchest()
-	minetest.show_formspec("inventory:enderchest", enderchest_formspec)
+function core.open_enderchest()
+	core.show_formspec("inventory:enderchest", enderchest_formspec)
 end
 
 -- HandSlot
 
 local hand_formspec = "size[9,8.75]"..
-	"label[0,0;"..minetest.formspec_escape(minetest.colorize("#313131", "Hand")).."]"..
+	"label[0,0;"..core.formspec_escape(core.colorize("#313131", "Hand")).."]"..
 	"list[current_player;hand;0,0.5;1,1;]"..
 	get_itemslot_bg(0,0.5,1,1)..
-	"label[0,4.0;"..minetest.formspec_escape(minetest.colorize("#313131", "Inventory")).."]"..
+	"label[0,4.0;"..core.formspec_escape(core.colorize("#313131", "Inventory")).."]"..
 	"list[current_player;main;0,4.5;9,3;9]"..
 	get_itemslot_bg(0,4.5,9,3)..
 	"list[current_player;main;0,7.74;9,1;]"..
@@ -94,12 +95,12 @@ local hand_formspec = "size[9,8.75]"..
 	"listring[current_player;hand]"..
 	"listring[current_player;main]"
 	
-function minetest.open_handslot()
-	minetest.show_formspec("inventory:hand", hand_formspec)
+function core.open_handslot()
+	core.show_formspec("inventory:hand", hand_formspec)
 end
 
-minetest.register_cheat("AutoEject", "Inventory", "autoeject")
-minetest.register_cheat("Hand", "Inventory", minetest.open_handslot)
-minetest.register_cheat("Enderchest", "Inventory", minetest.open_enderchest)
-minetest.register_cheat("AutoPlanks", "Inventory", "autoplanks")
-minetest.register_cheat("AutoRefill", "Inventory", "autorefill")
+core.register_cheat("AutoEject", "Inventory", "autoeject")
+core.register_cheat("Hand", "Inventory", core.open_handslot)
+core.register_cheat("Enderchest", "Inventory", core.open_enderchest)
+core.register_cheat("AutoPlanks", "Inventory", "autoplanks")
+core.register_cheat("AutoRefill", "Inventory", "autorefill")
--- builtin/cheats/worldutil.lua
+++ builtin/cheats/worldutil.lua
@@ -1,32 +1,60 @@
 -- Credits to Lizzy Fleckenstein
 -- lgpl license
 
-minetest.register_on_dignode(function(pos)
-	if minetest.settings:get_bool("replace") then
-		minetest.after(0, minetest.place_node, pos)
+core.register_on_dignode(function(pos)
+	if core.settings:get_bool("replace") then
+		core.after(0, core.place_node, pos)
 	end
 end)
 
 local etime = 0
+local uptime = 0
+local hud_id = nil
 
-minetest.register_globalstep(function(dtime)
+core.register_globalstep(function(dtime)
 	etime = etime + dtime
 	if etime < 1 then return end
-	local player = minetest.localplayer
+	local player = core.localplayer
 	if not player then return end
 	local pos = player:get_pos()
 	local item = player:get_wielded_item()
-	local def = minetest.get_item_def(item:get_name())
-	local nodes_per_tick = tonumber(minetest.settings:get("nodes_per_tick")) or 8
+	local def = core.get_item_def(item:get_name())
+	local nodes_per_tick = tonumber(core.settings:get("nodes_per_tick")) or 8
+
+	if item and item:get_count() > 0 and def and def.node_placement_prediction ~= "" and (core.settings:get_bool("scaffold") or core.settings:get_bool("scaffold_plus")) then
+        local total_items = core.get_total_items(item:get_name())
+        local player = core.localplayer
+        if player then
+            if hud_id then
+                player:hud_change(hud_id, "text", total_items .. " " .. item:get_description() .. " left")
+            else
+                hud_id = player:hud_add({
+                    type = "text",
+                    position = {x = 0.5, y = 0.5},
+                    offset = {x = 0, y = 25},
+                    text = total_items .. " " .. item:get_description() .. " left",
+                    alignment = {x = 0, y = 0},
+                    scale = {x = 150, y = 150},
+                    number = 0xF0F0F0
+                })
+            end
+        end
+    else
+        if hud_id and core.localplayer then
+            core.localplayer:hud_remove(hud_id)
+            hud_id = nil
+        end
+    end
+
 	if item and item:get_count() > 0 and def and def.node_placement_prediction ~= "" then
-		if minetest.settings:get_bool("scaffold") then
+		if core.settings:get_bool("scaffold") then
 			local p = vector.round(vector.add(pos, {x = 0, y = -0.6, z = 0}))
-			local node = minetest.get_node_or_nil(p)
-			if not node or minetest.get_node_def(node.name).buildable_to then
-				minetest.place_node(p)
+			local node = core.get_node_or_nil(p)
+			if not node or core.get_node_def(node.name).buildable_to then
+				core.place_node(p)
 			end
 		end
-		if minetest.settings:get_bool("scaffold_plus") then
+		if core.settings:get_bool("scaffold_plus") then
 			local z = pos.z
 			local positions = {
 				{x = 0, y = -0.6, z = 0},
@@ -40,46 +68,46 @@ minetest.register_globalstep(function(dtime)
                 {x = 1, y = -0.6, z = 1}
 			}
 			for i, p in pairs(positions) do
-				minetest.place_node(vector.add(pos, p))
+				core.place_node(vector.add(pos, p))
 			end
 		end
-		if minetest.settings:get_bool("block_water") then
-			local positions = minetest.find_nodes_near(pos, 5, {"mcl_core:water_source", "mcl_core:water_floating"}, true)
+		if core.settings:get_bool("block_water") then
+			local positions = core.find_nodes_near(pos, 5, {"mcl_core:water_source", "mcl_core:water_floating"}, true)
 			for i, p in pairs(positions) do
 				if i > nodes_per_tick then return end
-				minetest.place_node(p)
+				core.place_node(p)
 			end
 		end
-		if minetest.settings:get_bool("block_lava") then
-			local positions = minetest.find_nodes_near(pos, 5, {"mcl_core:lava_source", "mcl_core:lava_floating"}, true)
+		if core.settings:get_bool("block_lava") then
+			local positions = core.find_nodes_near(pos, 5, {"mcl_core:lava_source", "mcl_core:lava_floating"}, true)
 			for i, p in pairs(positions) do
 				if i > nodes_per_tick then return end
-				minetest.place_node(p)
+				core.place_node(p)
 			end
 		end
-		if minetest.settings:get_bool("autotnt") then -- little modification here, autoTNT now will only work if the player is holding tnt
-            minetest.switch_to_item("mcl_tnt:tnt") 
-			local positions = minetest.find_nodes_near_under_air_except(pos, 2, item:get_name(), true)
+		if core.settings:get_bool("autotnt") then -- little modification here, autoTNT now will only work if the player is holding tnt
+            core.switch_to_item("mcl_tnt:tnt") 
+			local positions = core.find_nodes_near_under_air_except(pos, 2, item:get_name(), true)
 			for i, p in pairs(positions) do
-                minetest.switch_to_item("mcl_tnt:tnt") 
+                core.switch_to_item("mcl_tnt:tnt") 
 				if i > nodes_per_tick then return end
-                    if minetest.switch_to_item("mcl_tnt:tnt") then
-				        minetest.place_node(vector.add(p, {x = 0, y = 1, z = 0}))
+                    if core.switch_to_item("mcl_tnt:tnt") then
+				        core.place_node(vector.add(p, {x = 0, y = 1, z = 0}))
                     else end
 			end
 		end
 	end
-	if minetest.settings:get_bool("nuke") then
+	if core.settings:get_bool("nuke") then
 		local i = 0
 		for x = pos.x - 4, pos.x + 4 do
 			for y = pos.y - 4, pos.y + 4 do
 				for z = pos.z - 4, pos.z + 4 do
 					local p = vector.new(x, y, z)
-					local node = minetest.get_node_or_nil(p)
-					local def = node and minetest.get_node_def(node.name)
+					local node = core.get_node_or_nil(p)
+					local def = node and core.get_node_def(node.name)
 					if def or def.diggable then
 						if i > nodes_per_tick then return end
-						minetest.dig_node(p)
+						core.dig_node(p)
 						i = i + 1
 					end
 				end
@@ -88,10 +116,10 @@ minetest.register_globalstep(function(dtime)
 	end
 end)
 
-minetest.register_cheat("Scaffold", "World", "scaffold")
-minetest.register_cheat("ScaffoldPlus", "World", "scaffold_plus")
-minetest.register_cheat("BlockWater", "World", "block_water")
-minetest.register_cheat("BlockLava", "World", "block_lava")
-minetest.register_cheat("AutoTNT", "World", "autotnt")
-minetest.register_cheat("Nuke", "World", "nuke")
-minetest.register_cheat("Replace", "World", "replace")
+core.register_cheat("Scaffold", "World", "scaffold")
+core.register_cheat("ScaffoldPlus", "World", "scaffold_plus")
+core.register_cheat("BlockWater", "World", "block_water")
+core.register_cheat("BlockLava", "World", "block_lava")
+core.register_cheat("AutoTNT", "World", "autotnt")
+core.register_cheat("Nuke", "World", "nuke")
+core.register_cheat("Replace", "World", "replace")
